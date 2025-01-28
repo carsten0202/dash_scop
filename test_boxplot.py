1b4fda03672cc52c241ae281d8d3758b18673d00
Carsten Rundsten <carsten.rundsten@gmail.com>
2025-01-28 10:37:20 +0100

    Update

--- test_boxplot.py
+++ test_boxplot.py
@@ -1,11 +1,19 @@
 
-import rpy2.robjects as ro
-from rpy2.robjects import r
-#import pandas as pd
+import pandas as pd
+
+rds_file = "testdata/20220818_brain_10x-test_rna-seurat.rds"
+csv_file = "testdata/20220818_brain_10x-test_rna-seurat.csv"
+
+df = pd.read_csv(csv_file, index_col="Unnamed: 0", usecols=range(26)).transpose()
+mygenes = df.columns
+df = df.reset_index().melt(id_vars=["index"], value_vars=df.columns, var_name="Gene", value_name="Counts")
+
+
+#import rpy2.robjects as ro
+#from rpy2.robjects import r
 
 # Load the RDS file
 # try:
-# 	rds_file = "testdata/20220818_brain_10x-test_rna-seurat.rds"
 # 	r_object = ro.r['readRDS'](rds_file)
 	# If the R object is a data frame, you can convert it to a pandas DataFrame
 #	if isinstance(r_object, ro.vectors.DataFrame):
@@ -13,30 +21,12 @@ from rpy2.robjects import r
 # except:
 # 	print("Unable to read RDS file")
 
-testgenes = ['Lypla1', 'Tcea1', 'Gm16041', 'Atp6v1h', 'Oprk1', 'Rb1cc1', 'Alkal1', 'Arl14ep', 'Fshb', 'Gm13912']
-
-import pandas as pd
-df = pd.read_csv("testdata/20220818_brain_10x-test_rna-seurat.csv", index_col="RowNames", usecols = lambda x: x not in ["Unnamed: 0"]).transpose()
-
 import dash
 from dash import dcc, html, callback, Output, Input
 import plotly.express as px
 
-# Example DataFrame
-# df = pd.DataFrame({
-#     "Gene": ["A", "A", "A", "B", "B", "B", "C", "C", "C"],
-#     "Counts": [10, 12, 11, 15, 14, 16, 20, 21, 19]
-# })
 
-df = df.reset_index().melt(id_vars=["index"], value_vars=testgenes, var_name="Gene", value_name="Counts")
-
-print(testgenes[0:3])
-print(df.loc[df['Gene'].isin(testgenes[0:3])])
-
-# Create the box plot
-# fig = px.box(df, x="Gene", y="Counts", title="Box and Whiskers Plot", log_y=True)
-
-# Dash App
+# Prep the Dash App
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
@@ -45,10 +35,10 @@ app.layout = html.Div([
     dcc.Dropdown(
         id="variable-dropdown",
         options=[
-            {"label": col, "value": col} for col in testgenes
+            {"label": col, "value": col} for col in mygenes
         ],
         multi=True,  # Enable multiple selection
-        value=testgenes,  # Default value
+        value=mygenes[0:3],  # Default value
         placeholder="Select a column to plot"
     ),
     dcc.Graph(id="box-plot")
@@ -75,8 +65,6 @@ def update_plot(selected_columns):
         log_y=True  # Logarithmic scale
     )
     return fig
-
-
 
 if __name__ == "__main__":
     app.run_server(debug=True)
