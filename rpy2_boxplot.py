28d7b6aacef31456a0f743d7b3f2be776f7673b1
Carsten Rundsten <carsten.rundsten@gmail.com>
2025-02-06 15:12:53 +0100

    Update rpy2_boxplot.py

--- rpy2_boxplot.py
+++ rpy2_boxplot.py
@@ -5,7 +5,6 @@ host = "0.0.0.0"
 port = 61040
 
 rds_file = "testdata/20220818_brain_10x-test_rna-seurat.rds"
-csv_file = "testdata/20220818_brain_10x-test_rna-seurat.csv"
 met_file = "testdata/seurat_metadata.csv"
 
 # Load the RDS file
