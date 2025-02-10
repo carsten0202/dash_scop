fa610bc8754b2cfbdbcce66cd3b90dd96e2c2720
Carsten Rundsten <carsten.rundsten@gmail.com>
2025-02-10 15:27:05 +0100

    Create director.py

---
+++ src/director/director.py
@@ -0,0 +1,14 @@
+
+class director(object):
+	def __init__(self, *args, **kwargs):
+		self._data = kwargs
+
+
+Options = {
+    "host": "0.0.0.0",
+    "port": 61020,
+    "x": "Cell Types",
+    "y": "Expression Counts",
+    "title": "Violin Plot Example",
+}
+
