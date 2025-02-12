ec6f21a64467296fe5c0cb74aa8153895f0da0dd
Carsten Rundsten <carsten.rundsten@gmail.com>
2025-02-12 13:14:42 +0100

    Update test_scanpy.py

--- test_scanpy.py
+++ test_scanpy.py
@@ -7,8 +7,3 @@ adata = sc.read_h5ad("testdata/seurat_data.h5ad")
 print(adata)
 print(adata.obsm)
 print(adata.obsm['X_umap'])
-
-# Now you can use Plotly and Dash for visualization
-import plotly.express as px
-# fig = px.scatter(x=adata.obsm['X_pca'][:, 0], y=adata.obsm['X_pca'][:, 1], color=adata.obs['cell_type'])
-# fig.show()
