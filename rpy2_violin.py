4e4ff782d1f61d03e0e925216486c811bf422726
Carsten Rundsten <carsten.rundsten@gmail.com>
2025-02-18 14:13:08 +0100

    Update rpy2_violin.py

--- rpy2_violin.py
+++ rpy2_violin.py
@@ -1,5 +1,7 @@
-
 import numpy as np
+import plotly.express as px
+from dash import Dash, Input, Output, dcc, html
+from rpy2.robjects import pandas2ri, r
 
 Options = {
     "host": "0.0.0.0",
@@ -12,46 +14,47 @@ Options = {
 rds_file = "testdata/20220818_brain_10x-test_rna-seurat.rds"
 
 # Load the RDS file
-from rpy2.robjects import r, pandas2ri
 try:
-    robj = r['readRDS'](rds_file)
-    rdgC = r['LayerData'](robj, assay = "SCT", layer = "counts")
+    robj = r["readRDS"](rds_file)
+    rdgC = r["LayerData"](robj, assay="SCT", layer="counts")
 except Exception as e:
-	print(e)
-	print("Unable to read RDS file")
+    print(e)
+    print("Unable to read RDS file")
 
 pandas2ri.activate()
-df = pandas2ri.rpy2py(r['as.data.frame'](rdgC))
-df = df.reset_index().melt(id_vars=["index"], value_vars=df.columns, var_name=Options["x"], value_name=Options["y"])
-with np.errstate(divide='ignore'):
-    df[Options["y"]] = np.log10(df[Options["y"]]) # Logarithmic scale
+df = pandas2ri.rpy2py(r["as.data.frame"](rdgC))
+df = df.reset_index().melt(
+    id_vars=["index"],
+    value_vars=df.columns,
+    var_name=Options["x"],
+    value_name=Options["y"],
+)
+with np.errstate(divide="ignore"):
+    df[Options["y"]] = np.log10(df[Options["y"]])  # Logarithmic scale
 
 
 # Dash App
-from dash import Dash, dcc, html, Output, Input
-import plotly.express as px
 app = Dash(__name__)
 
 categories = df[Options["x"]].unique()
-app.layout = html.Div([
-    html.H1("Violin Plot in Dash & Plotly"),
+app.layout = html.Div(
+    [
+        html.H1("Violin Plot in Dash & Plotly"),
+        # Dropdown for selecting variables to plot
+        dcc.Dropdown(
+            id="violin-dropdown",
+            multi=True,  # Enable multiple selection
+            options=[{"label": col, "value": col} for col in categories],
+            placeholder="Select a column to plot",
+            value=categories[0:10],  # Default value
+        ),
+        # The Graph
+        dcc.Graph(
+            id="violin-plot",
+        ),
+    ]
+)
 
-    # Dropdown for selecting variables to plot
-    dcc.Dropdown(
-        id="violin-dropdown",
-        multi=True,  # Enable multiple selection
-        options=[
-            {"label": col, "value": col} for col in categories
-        ],
-        placeholder="Select a column to plot",
-        value=categories[0:10],  # Default value
-    ),
-
-    # The Graph
-    dcc.Graph(
-        id="violin-plot",
-    )
-])
 
 @app.callback(
     Output(component_id="violin-plot", component_property="figure"),
@@ -78,5 +81,5 @@ def update_plot(selected_columns):
 
 
 # Run Server
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(host=Options["host"], port=Options["port"], debug=True)
