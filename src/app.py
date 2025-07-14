c1a16cfd28b796469913070636de55e64fa050c5
Carsten Rundsten <47785543+carsten0202@users.noreply.github.com>
2025-07-14 12:38:54 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,11 +3,10 @@ import os
 from dash import Dash
 
 from callbacks import register_callbacks
-from layout import layout
+from layout import get_layout
 
 # Initialize Dash app
 app = Dash(__name__)
-app.layout = layout
 
 # Register callbacks
 register_callbacks(app)
@@ -18,6 +17,7 @@ def main(config_data):
     port = str(os.getenv("DASH_PORT", 8050))
     debug = os.getenv("DASH_DEBUG", "True") == "True"
 
+    app.layout = get_layout(config_data)
     app.run(host=ip, port=port, debug=debug)
 
 
