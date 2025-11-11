99592c394130b7bfc554dd4f6c9ac571608e3def
Carsten Rundsten <carsten.rundsten@gmail.com>
2025-11-11 14:59:18 +0100

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(os.getenv("BASE_DIR", "testdata")).resolve(strict=False)  # Base directory for data files
-RDS_ALLOWED_EXT = {".rds", ".rda", ".rdata", ".csv"}  # Allowed file extensions
+RDS_ALLOWED_EXT = {".rds", ".rda", ".rdata"}  # Allowed file extensions
 
 max_features = 60  # Maximum number of features to plot at once
