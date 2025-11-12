aeaed8a14393c803e269357e1e57b119d4af3287
Carsten Rundsten <carsten.rundsten@gmail.com>
2025-11-12 10:55:04 +0100

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,13 @@
 import os
 from pathlib import Path
 
+# CLI Defaults
+DEFAULT_IP = os.getenv("DASH_IP", "127.0.0.1")
+DEFAULT_PORT = int(os.getenv("DASH_PORT", 8050))
+DEFAULT_DEBUG = os.getenv("DASH_DEBUG", "True") == "True"
+DEFAULT_RDS_FILE = "testdata/seurat_obj_downsampled.rds"
+
+# Other Settings
 BASE_DIR = Path(os.getenv("BASE_DIR", "testdata")).resolve(strict=False)  # Base directory for data files
 RDS_ALLOWED_EXT = {".rds", ".rda", ".rdata"}  # Allowed file extensions
 
