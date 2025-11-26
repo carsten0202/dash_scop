3e145c92bdf86d0234ba37c3020d81bb78f59770
Carsten Rundsten <carsten.rundsten@gmail.com>
2025-11-26 13:59:38 +0100

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,5 +1,6 @@
 import os
 
+import pandas as pd
 import rpy2.robjects as ro
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
@@ -33,5 +34,24 @@ def load_seurat_rds(file_path: str | os.PathLike[str], assay="SCT", layer="data"
         gene_matrix_df = extracted[1]  # Gene expression matrix as pandas DataFrame
         umap_df = extracted[2]  # UMAP data as pandas DataFrame...
         umap_df.columns = umap_df.columns.str.upper()  # ...and set column names to uppercase
+
+        gene_matrix_melted = gene_matrix_df.melt(ignore_index=False)
+
+        # Make barcodes categorical
+        gene_matrix_melted["variable"] = gene_matrix_melted["variable"].astype("category")
+        # Get the unique barcodes in the order pandas uses internally
+        barcodes = gene_matrix_melted["variable"].cat.categories
+        print("barcodes:", barcodes)
+        # Align metadata to those barcodes ONCE
+        cell_meta = metadata_df.reindex(barcodes)  # index: barcodes, length = n_cells
+        print(f"cell_meta: {cell_meta}")
+
+        print(f"gene_matrix_melted: {gene_matrix_melted}")
+        print(f"metadata_df.cols: {metadata_df.columns}")
+
+        meta_aligned = metadata_df.reindex(gene_matrix_melted["variable"]).reset_index(drop=True)
+        print(f"meta_aligned: {meta_aligned}")
+        gene_matrix_df_new = pd.concat([gene_matrix_melted.reset_index(drop=True), meta_aligned], axis=1)
+        print(f"gene_matrix_new: {gene_matrix_df_new}")
 
         return {"gene_counts": gene_matrix_df, "metadata": metadata_df, "umap": umap_df}
