fd67a657f07ded3456d775a332b030a07e69f92b
Carsten Rundsten <carsten.rundsten@gmail.com>
2025-12-18 15:54:33 +0100

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -7,8 +7,7 @@ DEFAULT_PORT = int(os.getenv("DASH_PORT", 8050))
 DEFAULT_DEBUG = os.getenv("DASH_DEBUG", "True") == "True"
 DEFAULT_RDS_PATH = os.getcwd()  # Default RDS file path is current working directory
 
-# Other Settings - BASE_DIR works poorly - should eliminate its usage where possible
-# BASE_DIR = Path(os.getenv("DASH_RDS_PATH", DEFAULT_RDS_PATH)).resolve(strict=False)  # Base directory for data files
+# Other Settings
 RDS_ALLOWED_EXT = {".rds", ".rda", ".rdata"}  # Allowed file extensions
 CACHE_DEFAULT_TIMEOUT = 12 * 60 * 60  # Cache timeout in seconds
 
