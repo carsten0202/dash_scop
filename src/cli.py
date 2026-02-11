9884fa0d3169f93cf4e541b55bcc28f6131341aa
Carsten Rundsten <47785543+carsten0202@users.noreply.github.com>
2026-02-11 15:14:42 +0100

    Update cli.py

--- src/cli.py
+++ src/cli.py
@@ -8,7 +8,7 @@ from settings import DEFAULT_DEBUG, DEFAULT_IP, DEFAULT_PORT, DEFAULT_RDS_PATH
 
 
 @click.command()
-@click.option("--config", type=click.Path(exists=True), default=".yml", callback=load_config, expose_value=False, is_eager=True, help="Path to a JSON or YAML config file.")
+@click.option("--config", type=click.Path(), default=".yml", callback=load_config, expose_value=False, is_eager=True, help="Path to a JSON or YAML config file.")
 @click.option("--debug/--no-debug", is_flag=True, default=DEFAULT_DEBUG, help="Enable Dash debug mode.")
 @click.option("--ip", default=DEFAULT_IP, help="IP address to run the Dash app on.")
 @click.option("--port", type=int, default=DEFAULT_PORT, help="Port number for the Dash app.")
@@ -18,7 +18,7 @@ def cli(ctx, debug, ip, port, rds_path):
     """Launch the Dash app with configurable IP, port, and debug mode."""
 
     # Set env vars from config file, then update with CLI args (CLI > config > defaults)
-    os.environ.update({k:str(v) for k,v in ctx.default_map.items()}) # v must be strings to be set as env vars
+    os.environ.update({k:str(v) for k,v in ctx.default_map.items() if ctx.default_map}) # v must be strings to be set as env vars
     os.environ["DATASCOPE_IP"] = ip
     os.environ["DATASCOPE_PORT"] = str(port)
     os.environ["DATASCOPE_DEBUG"] = str(debug)
