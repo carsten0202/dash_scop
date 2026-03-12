d771afc7fd5ef96cff6fa24e8d14c681cd20570f
Carsten Rundsten <47785543+carsten0202@users.noreply.github.com>
2026-03-12 14:34:07 +0100

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,7 +5,6 @@ import rpy2.robjects as ro
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
 from rpy2.robjects.packages import importr
-from scipy.stats import zscore
 
 # Load R packages
 try:
